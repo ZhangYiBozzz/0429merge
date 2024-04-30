23346bd74b7f8eee6d981a920e50ee88bffe6726
drean_0 <595977547@qq.com>
2024-04-30 11:49:10 +0800

    zyf v4.0

--- uppercoding/src/views/information.vue
+++ uppercoding/src/views/information.vue
@@ -72,7 +72,7 @@
                 class="h-[35px] w-[35px] p-[5px] rounded-[3px] hover:bg-[#e5ebfd] hover:text-[#3455db] cursor-pointer"
               />
             </li>
-            <li class="flex items-center cursor-pointer ">
+            <li class="flex items-center cursor-pointer">
               <a-avatar
                 class="mr-[14px]"
                 v-if="userName"
@@ -224,80 +224,7 @@
         </a-menu>
       </a-layout-sider>
       <a-layout-content class="w-[100%] h-[90vh] bg-[#fff]">
-        <!-- <router-view></router-view> -->
-        <div class="px-[48px] my-[16px]">
-          <!-- 分支与搜索 -->
-          <ul class="flex items-center justify-between h-[32px]">
-            <li class="h-[32px] w-[132px] flex justify-between items-center">
-              <div class="mr-[16px]">
-                <a-select
-                  default-value="master"
-                  style="width: 120px"
-                  @change="handleChange"
-                >
-                  <a-select-option
-                    v-for="(item, index) in selecrBranch"
-                    :key="index"
-                    :value="item.name"
-                  >
-                    {{ item.name }}
-                  </a-select-option>
-                </a-select>
-              </div>
-              <span>abc</span>
-            </li>
-            <li class="h-[32px]">
-              <a-input ref="userNameInput" class="w-[250px]">
-                <a-icon slot="prefix" type="search" />
-              </a-input>
-            </li>
-          </ul>
-          <!-- 更新时间 -->
-          <ul
-            class="h-[83px] w-[100%] mt-[16px] border-[1px] flex justify-between items-center border-solid border-[#e3e3e3] box-border px-[16px] pt-[4px]"
-          >
-            <li>
-              <p
-                class="mb-[4px] text-[#5b82b6]"
-                v-if="aBranch && aBranch.commit && aBranch.commit.commit"
-              >
-                {{ aBranch.commit.commit.message }}
-              </p>
-              <p class="mb-[4px]" v-else>加载中...</p>
-              <div class="flex items-center">
-                <a-avatar size="small" icon="user" class="mr-[8px]" />
-                <p
-                  class="mb-[4px]"
-                  v-if="aBranch && aBranch.commit && aBranch.commit.commit"
-                >
-                  {{ aBranch.commit.commit.committer.email }}
-                </p>
-                <p class="mb-[4px]" v-else>加载中...</p>
-                <Icon
-                  icon="material-symbols:help-outline"
-                  style="color: #5b82b6"
-                  class="w-[14px] h-[14px] mx-[8px]"
-                />
-                <p>编辑于:{{ formattedCommitDate }}</p>
-              </div>
-            </li>
-            <li class="h-[32px]">
-              <a-button>
-                <template>
-                  <div class="relative flex w-[150px]">
-                    <p>aa3148d9</p>
-                    <div class="absolute flex right-0 top-[4px]">
-                      <p
-                        class="w-[1px] h-[33px] bg-[#e4e4e4] absolute left-[-13px] top-[-9px]"
-                      ></p>
-                      <Icon icon="zondicons:copy" style="color: #636e94" />
-                    </div>
-                  </div>
-                </template>
-              </a-button>
-            </li>
-          </ul>
-        </div>
+        <router-view></router-view>
       </a-layout-content>
     </a-layout>
   </a-layout>
@@ -314,7 +241,6 @@ import {
   pullsAll,
   userInfo,
 } from "@/service";
-import dayjs from "dayjs";
 
 export default {
   async created() {
@@ -400,38 +326,6 @@ export default {
       } else {
         this.openKeys = latestOpenKey ? [latestOpenKey] : [];
       }
-    },
-    handleChange(value) {
-      this.branch = value;
-    },
-  },
-  computed: {
-    formattedCommitDate() {
-      // 确保aBranch.commit.commit.committer.date存在
-      if (
-        this.aBranch &&
-        this.aBranch.commit &&
-        this.aBranch.commit.commit &&
-        this.aBranch.commit.commit.committer &&
-        this.aBranch.commit.commit.committer.date
-      ) {
-        return dayjs(this.aBranch.commit.commit.committer.date).format(
-          "YYYY-MM-DD HH:mm:ss"
-        ); // 使用你想要的格式
-      }
-      return "No date available"; // 如果日期不存在，返回一个默认值
-    },
-  },
-  watch: {
-    async branch() {
-      this.aBranch = (
-        await aBranch({
-          owner: this.owner,
-          repo: this.repo,
-          branch: this.branch,
-        })
-      ).data;
-      console.log(this.aBranch);
     },
   },
 };
